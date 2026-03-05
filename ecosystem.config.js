64eee39dfe52f5263f686fabc293670cfe0936e0
kashif-ahmad-smartitbox <tech2@smartitbox.in>
2026-03-05 13:45:31 +0530

    task : some changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,14 @@ module.exports = {
         {
             name: "smartitbox-frontend",
             script: "node_modules/next/dist/bin/next",
-            args: "start",
+            args: "start -p 3001",
             cwd: "./",
             instances: 1,
             autorestart: true,
             watch: false,
+            env: {
+                PORT: 3001
+            }
         }
     ]
 }
